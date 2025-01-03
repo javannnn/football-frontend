dde24be88f396d2207402c33b8cef75f79d201a3
Yafet Surafel <wedeyafet@gmail.com>
2025-01-03 14:18:38 +0300

    Fix Tailwind config and install react-router-dom

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Include all JavaScript and TypeScript files in the src folder
+    "./public/index.html",       // Include your public index.html
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
 
